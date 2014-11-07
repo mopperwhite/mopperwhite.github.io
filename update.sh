2e5b5053a7d605fc58825dfcd7afdfcf628ea845
mopperwhite <mopperwhite@gmail.com>
2014-11-07 21:19:27 +0800

    2014年 11月 07日 星期五 21:19:27 CST

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mv _config.yml local_config.yml
+mv online_config.yml _config.yml
+date > commit.txt
+git add -A
+git commit -F ./commit.txt
+git push https://github.com/mopperwhite/mopperwhite.github.io.git
+mv _config.yml online_config.yml
+mv local_config.yml _config.yml
